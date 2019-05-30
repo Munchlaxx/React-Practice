5843c6b3837f1ec994cf8095b593cdee9455fef4
Munchlaxx <evan.mirschel@gmail.com>
2019-05-30 09:48:42 -0400

    Initial Commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Melvin Mudbot',
+    username: 'Melvin',
+    email: 'melvin@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Jerry Jipster',
+    username: 'Jerry',
+    email: 'jipster@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Sulky Suspect',
+    username: 'Sulky',
+    email: 'sulky@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Billy Ballsack',
+    username: 'Billy',
+    email: 'billy.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Connor Creatine',
+    username: 'Connor',
+    email: 'connorr@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Musty Mallock',
+    username: 'Musty',
+    email: 'musty.info'
+  },
+  {
+    id: 7,
+    name: 'Lossy Laughter',
+    username: 'Lossy',
+    email: 'lossy@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Vegan Vengance',
+    username: 'Vegan',
+    email: 'vegan@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Marco Mustang',
+    username: 'Marco',
+    email: 'marco@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Alpha Ahole',
+    username: 'Alpha',
+    email: 'alpha@karina.biz'
+  }
+];
